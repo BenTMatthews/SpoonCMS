bd61919e340e3ed1358b2fe444a1080bf8d5beaa
Lemoneydes <patje009@gmail.com>
2018-08-18 11:01:36 +0200

    Hardcoded connstring

--- ExampleCore/Startup.cs
+++ ExampleCore/Startup.cs
@@ -24,7 +24,7 @@ namespace ExampleCore
         public void ConfigureServices(IServiceCollection services)
         {
             // LiteDB path to store file, for instance: "Data\\Spoon\\"   
-            string connString = Configuration["LiteDBSettings:ConnectionString"];
+            string connString = "Data\\Spoon\\";
             ISpoonData spoonData = SpoonWebWorker.GenerateDataWorker(SpoonDBType.LiteDB, connString);
 
             //Postgres DB connection string, for instance: "database=xxxx; host=xxx.xxx.xxx.xxx.com; username=xxx; password=xxx; SslMode=Prefer; port=1234;"
